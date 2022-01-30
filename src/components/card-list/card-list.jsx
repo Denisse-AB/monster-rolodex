957fe8bb03e997c5a8ec613c6d64c6ea3a0c592d
Denisse Abreu <denisseabreu21@yahoo.com>
2022-01-29 21:14:36 -0400

    first commit

---
+++ src/components/card-list/card-list.jsx
@@ -0,0 +1,14 @@
+// Component to render monsters object and
+// send then to the card component as a prop
+import React from "react";
+import {Card} from '../card/card';
+
+import './card-list.css';
+
+export const CardList = props => (
+  <div className="card-list">
+    {props.monsters.map(monster => (
+      <Card key={monster.id} monster={monster}/>))
+    }
+  </div>
+);
